a4c71faa1f93c107b0f35b4940fe7005a9216930
Owain Lewis <owain@owainlewis.com>
2015-11-05 21:55:14 +0000

    trifunctors

--- src/main/scala/io/forward/polyfunctors/Bifunctor.scala
+++ src/main/scala/io/forward/polyfunctors/Bifunctor.scala
@@ -1,30 +1,21 @@
 package io.forward.polyfunctors
 
 trait Bifunctor[F[+_, +_]] {
-  def bimap[A, B, C, D](k: F[A, B], f: A => C, g: B => D): F[C, D]
+  def bimap[A, B, C, D](domain: F[A, B], f: A => C, g: B => D): F[C, D]
 }
 
 object Bifunctor {
-
   def apply[F[_, _]](implicit F: Bifunctor[F]): Bifunctor[F] = F
 
   implicit def Tuple2Bifunctor: Bifunctor[Tuple2] = new Bifunctor[Tuple2] {
-    def bimap[A, B, C, D](k: (A, B), f: A => C, g: B => D) = (f(k._1), g(k._2))
+    def bimap[A, B, C, D](domain: (A, B), f: A => C, g: B => D) = (f(domain._1), g(domain._2))
   }
 
   implicit def EitherBifunctor: Bifunctor[Either] = new Bifunctor[Either] {
-    def bimap[A, B, C, D](k: Either[A, B], f: A => C, g: B => D) =
-      k match {
+    def bimap[A, B, C, D](domain: Either[A, B], f: A => C, g: B => D) =
+      domain match {
         case Left(a) => Left(f(a))
         case Right(b) => Right(g(b))
       }
   }
 }
-
-object BifunctorExamples {
-
-  val inc = (x: Int) => x + 1
-
-  Bifunctor[Tuple2].bimap((1,2), Foo.inc, Foo.inc)
-}
-
---
+++ src/main/scala/io/forward/polyfunctors/examples/Simple.scala
@@ -0,0 +1,11 @@
+package io.forward.polyfunctors.examples
+
+import io.forward.polyfunctors._
+
+object Simple {
+
+  val inc = (x: Int) => x + 1
+
+  val result = Bifunctor[Tuple2].bimap((1,2), inc, inc)
+}
+
