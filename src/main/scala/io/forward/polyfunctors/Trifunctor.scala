5fe2f9a986c4ec47465f9905462f51743045718c
Owain Lewis <owain@owainlewis.com>
2018-09-17 12:38:36 +0100

    Update Trifunctor.scala

--- src/main/scala/io/forward/polyfunctors/Trifunctor.scala
+++ src/main/scala/io/forward/polyfunctors/Trifunctor.scala
@@ -1,6 +1,7 @@
 package io.forward.polyfunctors
 
 case class Tri[A, B, C](a: A, b: B, c: C)
+
 trait Trifunctor[F[+ _, + _, + _]] {
   def trimap[A, B, C, X, Y, Z](domain: F[A, B, C], f: A => X, g: B => Y, h: C => Z): F[X, Y, Z]
 }
